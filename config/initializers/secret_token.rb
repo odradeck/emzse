902fd39dbdbe6be12409cf77e39bcd3d9e6503c2
odradeck <odradeck@gmail.com>
2013-04-30 18:36:14 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emzse::Application.config.secret_token = '2e4b2f6757e67d8fa1c4c5c0f57bb3bd88b3451cba661794e66f7222486a13cefbfee6990409836e2e72697674f739d43570f37aa58aedbff87761593d420676'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Emzse::Application.config.session_store :cookie_store, key: '_emzse_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Emzse::Application.config.session_store :active_record_store
